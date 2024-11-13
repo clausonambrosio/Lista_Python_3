ef8303615fccfa458947612c77f635760b7ce979
Clauson <clausonambrosio@hotmail.com>
2024-11-13 16:09:13 -0300

    desafio 1

---
+++ desafio_1.py
@@ -0,0 +1,21 @@
+from datetime import date, timedelta
+
+class Filme:
+    def __init__(self, titulo, genero, ano):
+        self.titulo = titulo
+        self.genero = genero
+        self.ano = ano
+        self.disponivel = True
+        
+    def exibir_informacoes(self):
+        return f"Título: {self.titulo}, Gênero: {self.genero}, Ano: {self.ano}, Status: {self.disponivel}."
+        
+    def alugar(self):
+        status = "Indisponível" if self.disponivel == False else "Disponível"
+        
+    def devolver(self):
+        status = "Disponível" if self.disponivel == True else "Emprestado"
+        
+        
+        
+        
